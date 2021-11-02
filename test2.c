4c08368c0efd389837bb1e285515f453bcfb0e11
charminglee <charminglee@jieztech.com>
2021-11-02 20:34:55 +0800

    change test2 case

--- test2.c
+++ test2.c
@@ -1,6 +1,7 @@
 #include "types.h"
 #include "user.h"
 #include "param.h"
+#include "memlayout.h"
 
 int
 main(int argc, char *argv[])
@@ -10,16 +11,16 @@ main(int argc, char *argv[])
     {
         sleep(10);
         char *shp = (char *)get_shared_page(1, 3);
-        printf(0, "process pid:%d read share pages content:%s\n", pid, shp);
+        printf(0, "process pid:%d read share pages address:%x content:%s\n", pid, V2P(shp), shp);
         free_shared_page(1);
         printf(0, "process pid:%d free share pages\n", pid);
     } else {
         char *shp = (char *)get_shared_page(1, 3);
         char *content = "hello";
         strcpy(shp, content);
-        printf(0, "process pid:%d write share pages content:%s\n", pid, content);
+        printf(0, "process pid:%d write share pages address:%x content:%s\n", pid, V2P(shp), content);
         wait();
-        printf(0, "process pid:%d read share pages content:%s\n", pid, shp);
+        printf(0, "process pid:%d read share pages address:%x content:%s\n", pid, V2P(shp), shp);
     }
     exit();
 }
